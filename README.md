69bfa66f013132d5ed327742e9ed4c327ecbf405
Andrew Cutler <andrew@panubo.com>
2019-02-14 15:09:35 +1100

    Add s3sync option

--- README.md
+++ README.md
@@ -5,9 +5,18 @@ Minimal Alpine Linux Docker image with `nginx` exposed, `awscli` and an s3 deplo
 This is useful as an intermediate container for  static site build artefacts. This image can be used
 as a container for static websites, and the resulting artefact can be viewed locally, or deployed to an S3 bucket.
 
+## Commands
+
+- `nginx` - Serve static files from `/var/www/html` (default)
+- `s3sync` - Synchronize the files in `/var/www/html` with s3 bucket
+
 ## Environment Options
 
--
+For s3sync:
+
+- `AWS_ACCESS_KEY` - AWS Access Key (required)
+- `AWS_SECRET_KEY` - AWS Secret Key (required)
+- `AWS_BUCKET_NAME` - AWS Bucket Name
 
 ## Status
 
--- entry.sh
+++ entry.sh
@@ -4,5 +4,31 @@ set -e
 
 [ "$DEBUG" == 'true' ] && set -x
 
+function s3sync() {
+  # Configuration checks
+  if [ -z "$AWS_BUCKET_NAME" ]; then
+      echo "Error: AWS_BUCKET_NAME is not specified"
+      exit 128
+  fi
+
+  if [ -z "$AWS_ACCESS_KEY" ]; then
+    echo "Error: AWS_ACCESS_KEY not specified"
+    exit 128
+  fi
+
+  if [ -z "$AWS_SECRET_KEY" ]; then
+      echo "Error: AWS_SECRET_KEY not specified"
+      exit 128
+  fi
+
+  # run the command
+  s3cmd --access_key=${AWS_ACCESS_KEY} --secret_key=${AWS_SECRET_KEY} sync /var/www/html s3://${AWS_BUCKET_NAME}
+}
+
 echo "Running $@..."
-exec "$@"
+
+if [ "${1}" == "s3sync" ]; then
+  s3sync
+else
+  exec "$@"
+fi
