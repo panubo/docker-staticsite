b85fab875683cff75f50db31a584c7678be3954f
Tim Robinson <tim@panubo.com>
2026-01-13 10:04:02 +1100

    Make bats simpler

--- tests/setup.bash
+++ tests/setup.bash
@@ -1,9 +1,7 @@
 # Load bats libraries
 
-BATS_LIB_PATH=${BATS_LIB_PATH:-"~/.bats/libs:/usr/lib/bats"}
-if BREW_PREFIX="$(brew --prefix)"; then
-  BATS_LIB_PATH="${BATS_LIB_PATH}:${BREW_PREFIX}/lib"
-fi
+# This list of PATHs should cover Linux, Mac and GitHub Actions
+BATS_LIB_PATH="~/.bats/libs:/opt/homebrew/lib:/usr/local/bats:/usr/local:/usr/lib/bats:/usr/lib"}
 export BATS_LIB_PATH
 bats_load_library bats-support
 bats_load_library bats-assert
